15b99a9815ffa47dd697195d485fef2e754e4879
Shivam Ingle <ingleshivam@gmail.com>
2024-01-31 12:58:38 +0530

    16/01/2024

---
+++ NestedLoopEx1/NestedLoopEx1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoopEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find the amstrong number form 1 to 500
+            int num, sum = 0, dig;
+            for (int i = 1; i <= 500; i++)
+            {
+                num = i;
+                while (num > 0)
+                {
+                    dig = num%10;
+                    sum = sum + (dig * dig * dig);
+                    num = num / 10;
+                }
+                if (i == sum)
+                {
+                    Console.WriteLine(i + " is a amstrong number");
+                }
+                sum = 0;
+            }
+        }
+    }
+}
---
+++ NestedLoopTable/NestedLoopTable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoopTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Program to display table form 2 to 5
+            int displayTable;
+            for(int i = 2; i <= 5; i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write(i*j+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
