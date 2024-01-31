bea4ce8363eb3b139d2a70162d669098dcb85a55
Shivam Ingle <ingleshivam@gmail.com>
2024-01-31 13:00:35 +0530

    13/01/2024

---
+++ ForLoopEx2/ForLoopEx2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int oddCount = 0;
+            for(int i = 10; i <= 40; i++)
+            {
+                if (i % 2 != 0) {
+                    Console.WriteLine(i);
+                    oddCount++;
+                }
+            }
+            Console.WriteLine("OddCount : " + oddCount);
+        }
+    }
+}
