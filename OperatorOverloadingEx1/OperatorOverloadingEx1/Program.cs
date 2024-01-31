dd3d9bbc47f3bee1bd7f18054705b2403d68391f
Shivam Ingle <ingleshivam@gmail.com>
2024-01-31 21:19:50 +0530

    31/01/2024

---
+++ OperatorOverloadingEx1/OperatorOverloadingEx1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingEx1
+{
+    class Weight
+    {
+        private int Kg;
+        private int Gm;
+
+        public void addWeight(int a, int b)
+        {
+            this.Kg = a;    
+            this.Gm = b;
+        }
+
+        public void displayWeight()
+        {
+            Console.WriteLine("Kg : " + this.Kg);
+            Console.WriteLine("Gm : " + this.Gm);
+        }
+
+        public static Weight operator +(Weight t1, Weight t2)
+        {
+            Weight temp = new Weight();
+            temp.Gm = t1.Gm + t2.Gm;
+            if(temp.Gm >= 1000)
+            {
+                temp.Kg = temp.Gm / 1000;
+                temp.Gm = temp.Gm % 1000;
+            }
+
+            temp.Kg = t1.Kg + t2.Kg + temp.Kg;
+            return temp;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weight w1 = new Weight();
+            w1.addWeight(10, 800);
+            w1.displayWeight();
+
+            Weight w2 = new Weight();
+            w2.addWeight(20, 800);
+            w2.displayWeight();
+
+            Weight w3 = w1 + w2;
+            w3.displayWeight();
+        }
+    }
+}
---
+++ OperatorOverlodingEx2/OperatorOverlodingEx2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverlodingEx2
+{
+    class Point
+    {
+        private int X;
+        private int Y;
+        
+        public void addPoint(int a , int b)
+        {
+            this.X = a;
+            this.Y = b;
+        }
+
+        public void displayPoint()
+        {
+            Console.WriteLine("X : " + X);
+            Console.WriteLine("Y : " + Y);
+        }
+
+        public static Point operator ++(Point p1)
+        {
+            p1.X++;
+            p1.Y++;
+
+            return p1;
+        }
+
+        public static Point operator --(Point p1)
+        {
+            p1.X--;
+            p1.Y--;
+
+            return p1;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.addPoint(2, 3);
+            Console.WriteLine("Initial Values");
+            p1.displayPoint();
+            p1++;
+            p1++;
+            p1++;
+            Console.WriteLine("After Increment by 3");
+            p1.displayPoint();
+            p1--;
+            p1--;
+            Console.WriteLine("After Decrement by 2");
+            p1.displayPoint();
+        }
+    }
+}
