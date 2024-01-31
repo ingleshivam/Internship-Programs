76c0dea18d554b357bf840d0e4a258e3c64b3b05
Shivam Ingle <ingleshivam@gmail.com>
2024-01-31 13:01:38 +0530

    12/01/2024

---
+++ NestedLoopsEx1/NestedLoopsEx1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoopsEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Enter the fist number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the second number : ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the third number : ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("Grater number is A :" + a);
+            }
+            else {
+                if (b > c)
+                {
+                    Console.WriteLine("Greate number is B :" + b);
+                }
+                else {
+                    Console.WriteLine("Greater number is C:" + c);
+                }
+            }
+
+        }
+    }
+}
---
+++ NestedLoopsEx2/NestedLoopsEx2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedLoopsEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int physics, chemistry, maths, bio, english, total;
+            double percentage;
+
+            Console.Write("Enter the marks for physics subject :");
+            physics = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the marks for chemistry subject :");
+            chemistry = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the marks for maths subject :");
+            maths = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the marks for bio subject :");
+            bio = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the marks for english subject :");
+            english = Convert.ToInt32(Console.ReadLine());
+
+            total = physics + chemistry + maths + bio + english;
+            percentage = (double) total / 5;
+            Console.WriteLine("Total :"+total);
+            Console.WriteLine("Percentage :" + percentage);
+
+            if (percentage >= 70)
+            {
+                Console.WriteLine("Distinction Class");
+            }
+            else {
+                if (percentage >= 60)
+                {
+                    Console.WriteLine("First Class");
+                }
+                else {
+                    if (percentage >= 50)
+                    {
+                        Console.WriteLine("Second Class");
+                    }
+                    else {
+                        if (percentage >= 40)
+                        {
+                            Console.WriteLine("Pass");
+                        }
+                        else {
+                            Console.WriteLine("Fail");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
